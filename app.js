3f2af32e95dc709cb58e1f24efbcc46447984db5
Pranav <pranavmendi@gmail.com>
2020-12-27 00:46:01 +0530

    Added compose blog page

--- app.js
+++ app.js
@@ -23,12 +23,13 @@ app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
 
-
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
 
 
