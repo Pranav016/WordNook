4adb65daf46d94b81f43bd91b834b69395480891
Pranav <pranavmendi@gmail.com>
2020-12-27 02:40:14 +0530

    Added functionality to post blogs to home page

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+const posts = [];
 
 app.get("/", function(req, res){
-  res.render("home", {homeStartingContent: homeStartingContent});
+  res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function(req, res){
@@ -36,6 +37,8 @@ app.post("/compose", function(req, res){
     title: req.body.postTitle,
     content: req.body.postBody,
   };
+  posts.push(post);
+  res.redirect("/");
 });
 
 
