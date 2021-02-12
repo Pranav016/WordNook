31b6887faaaa7057e21b5465cebb329c3efbbae2
Pranav <pranavmendi@gmail.com>
2021-02-12 17:12:39 +0530

    Remove redundant actions and update user model path

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/user.model');
+const User = require('../models/User.model');
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const auth = require("../middlewares/auth");
