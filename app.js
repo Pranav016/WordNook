83327f619c5bcd4fd7ed4d7028315398b2179c74
pankaj kumar bijarniya <59636849+pankajkumarbij@users.noreply.github.com>
2021-02-22 22:47:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 //Connecting to Mongo Database using ODM Mongoose-
-const URL = 'mongodb://localhost:27017/blogDB';
+const URL = process.env.URL;
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Setting up schema for the collection-
