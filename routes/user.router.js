b918459cb662d1b20a1fb48f8bedc6b77a4f2ca7
Ayush Jain <ayushjain17.n@gmail.com>
2021-04-07 13:14:52 +0530

    prettier formatting

--- routes/user.router.js
+++ routes/user.router.js
@@ -317,7 +317,10 @@ router.get('/author/:id', auth, async (req, res) => {
                     toggleunfollow = true;
                 }
             });
-            const likedBlogs = await Blog.find({ '_id' : {$in : user.likedPosts}, status:'Public'});
+            const likedBlogs = await Blog.find({
+                _id: { $in: user.likedPosts },
+                status: 'Public',
+            });
             const blogs = await Blog.find({
                 author: req.params.id,
                 status: 'Public',
@@ -330,7 +333,7 @@ router.get('/author/:id', auth, async (req, res) => {
                 toggleunfollow,
                 posts: blogs,
                 isAuthenticated: !!req.user,
-                likedBlogs: likedBlogs
+                likedBlogs: likedBlogs,
             });
         } catch (error) {
             return res.redirect('/error');
@@ -353,13 +356,15 @@ router.get('/dashboard', auth, async (req, res) => {
                 .sort({ timestamps: 'desc' })
                 .lean();
             const allusers = await User.find({});
-            const likedBlogs = await Blog.find({ '_id' : {$in : user.likedPosts}});
+            const likedBlogs = await Blog.find({
+                _id: { $in: user.likedPosts },
+            });
             return res.render('dashboard', {
                 user,
                 allusers,
                 posts: blogs,
                 isAuthenticated: !!req.user,
-                likedBlogs: likedBlogs
+                likedBlogs: likedBlogs,
             });
         } catch (error) {
             return res.redirect('/error');
