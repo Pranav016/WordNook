8b2fb9a5c21e917133baff4e1a5578fb723b8069
Pranav <54665036+Pranav016@users.noreply.github.com>
2021-04-24 03:42:13 +0530

    Update landBot.js

--- public/js/landBot.js
+++ public/js/landBot.js
@@ -1,4 +1,4 @@
 const myLandbot = new Landbot.Livechat({
 	configUrl:
-		'https://chats.landbot.io/v3/H-812017-18RWUI6DKMDDS0XR/index.json',
+		`https://chats.landbot.io/v3/${process.env.LANDBOT_KEY}/index.json`,
 });
