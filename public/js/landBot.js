795e5f8a5dd60c18661fbe9fdd39cd3ae648bfe3
Pranav <pranavmendi@gmail.com>
2021-04-26 06:19:26 +0530

    Update chatbot URL

--- public/js/landBot.js
+++ public/js/landBot.js
@@ -1,3 +1,3 @@
 const myLandbot = new Landbot.Livechat({
-	configUrl: `https://chats.landbot.io/v3/${process.env.LANDBOT_KEY}/index.json`,
+	configUrl: `https://chats.landbot.io/v3/H-812017-18RWUI6DKMDDS0XR/index.json`,
 });
