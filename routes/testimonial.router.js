61cbb09d8280dc6559be799b44d4c4f3d4ea7dae
theBeginner86 <tobblex@hotmail.com>
2021-05-01 10:43:26 +0530

    Added Testimonial Wall

--- routes/testimonial.router.js
+++ routes/testimonial.router.js
@@ -2,26 +2,39 @@ const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const auth = require('../middlewares/auth');
+const testimonials = require('../dummy-data/testimonial');
+
 
 const router = express.Router();
 router.use(methodOverride('_method'));
 router.use(bodyParser.json());
-// Get request for testimonial-write page-
-router.get('/testimonial-write', auth, async (req, res) => {
-	const { user } = req;
-	if (!user) {
-		return res.status(401).redirect('/log-in');
-	}
-	res.render('./postitems/testimonial-write', {
-		isAuthenticated: true,
+
+// Get request for testimonial-wall
+router.get('/testimonial-wall', auth, async (req, res) => {
+	console.log(testimonials);
+	res.render('./postitems/testimonial-wall', {
+		isAuthenticated: !!req.user,
+		testimonials,
 	});
-	res.redirect('/');
-});
-router.post('/testimonial-write', auth, async (req, res) => {
-	const { user } = req;
-	if (!user) {
-		return res.status(401).redirect('/log-in');
-	}
-	res.redirect('/');
-});
+}); 
+
+//Will reuse this in next feature.
+// // Get request for testimonial-write page-
+// router.get('/testimonial-write', auth, async (req, res) => {
+// 	const { user } = req;
+// 	if (!user) {
+// 		return res.status(401).redirect('/log-in');
+// 	}
+// 	res.render('./postitems/testimonial-write', {
+// 		isAuthenticated: true,
+// 	});
+// 	res.redirect('/');
+// });
+// router.post('/testimonial-write', auth, async (req, res) => {
+// 	const { user } = req;
+// 	if (!user) {
+// 		return res.status(401).redirect('/log-in');
+// 	}
+// 	res.redirect('/');
+// });
 module.exports = router;
