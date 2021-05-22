a408c52b91b8f69f937baa6a681d895237fb49a9
Ayush Jain <ayushjain17.n@gmail.com>
2021-05-22 15:11:25 +0530

    Fixed bug, show replies button was working only for first comment, now working for all

--- public/js/commentReply.js
+++ public/js/commentReply.js
@@ -1,17 +1,21 @@
-const replyToggleBtn = document.getElementById('reply-toggle');
-const replyDialog = document.getElementById('reply-id');
-replyToggleBtn.addEventListener('click', function (e) {
-	if (this.innerHTML === 'Show Replies') {
-		this.innerHTML = 'Hide Replies';
-		showReplies();
-	} else {
-		this.innerHTML = 'Show Replies';
-		hideReplies();
-	}
-});
-function showReplies() {
+const replyToggleBtns = document.getElementsByClassName('reply-toggle');
+
+for (let i = 0; i < replyToggleBtns.length; i++) {
+	replyToggleBtns[i].addEventListener('click', function (e) {
+		if (this.innerHTML === 'Show Replies') {
+			this.innerHTML = 'Hide Replies';
+			showReplies(i);
+		} else {
+			this.innerHTML = 'Show Replies';
+			hideReplies(i);
+		}
+	});
+}
+function showReplies(i) {
+	const replyDialog = document.getElementById(`reply-id-${i}`);
 	replyDialog.classList.add('show');
 }
-function hideReplies() {
+function hideReplies(i) {
+	const replyDialog = document.getElementById(`reply-id-${i}`);
 	replyDialog.classList.remove('show');
 }
