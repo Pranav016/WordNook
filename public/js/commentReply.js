81ed601c87ba91d331a065ca1f212473da5b8abb
Sukriti-sood <soodsukriti1111@gmail.com>
2021-05-31 07:22:59 +0530

    tested husky

--- public/js/commentReply.js
+++ public/js/commentReply.js
@@ -11,6 +11,7 @@ for (let i = 0; i < replyToggleBtns.length; i++) {
 		}
 	});
 }
+
 function showReplies(i) {
 	const replyDialog = document.getElementById(`reply-id-${i}`);
 	replyDialog.classList.add('show');
